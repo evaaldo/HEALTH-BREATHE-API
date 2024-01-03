fe516a53a90d8d090b17d508cc22ea1c113be7ba
evaldo <evaaldormj@gmail.com>
2024-01-03 16:44:35 -0300

    Feat: Model usuário

---
+++ Models/Usuarios.cs
@@ -0,0 +1,10 @@
+namespace HealthBreath.Models
+{
+    public class Usuario
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+        public bool Administrador { get; set; }
+    }
+}
