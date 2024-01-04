88203732a09ecc9b743aedabe70e675c5a7135c6
evaldo <evaaldormj@gmail.com>
2024-01-04 16:01:07 -0300

    Feat: Conexão com BancoContext

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using HealthBreath.Context.BancoContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BancoContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoDietas")));
+builder.Services.AddDbContext<BancoContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoUsuarios")));
+builder.Services.AddDbContext<BancoContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoTreinos")));
 
 var app = builder.Build();
 
